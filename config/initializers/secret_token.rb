2bd1971ac17d50adfecdf4f929e0dc2816395674
Christian Frugard <christian.frugard@sidera.net>
2011-09-06 08:25:09 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railstutorial2::Application.config.secret_token = '98f800c814f95d7ff11d105a220af386c72262e688d92cbcaf14bc79d42419bbe89c643331682004997f6b17e4aa31549f570aa59df8b7150677eef14bdbd495'
