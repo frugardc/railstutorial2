27837b5a04d4e46129421f2e472d1fb7e8d66f34
Christian Frugard <frugardc@gmail.com>
2011-09-14 07:29:52 -0400

    adding basic styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def title
+		base_title = "Ruby on Rails Sample Tutorial"
+		if @title.nil?
+			return base_title
+		else
+			return "#{base_title} | #{@title}"
+		end
+	end
 end
